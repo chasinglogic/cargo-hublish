8dcb6b8f0f167b17546310dcad8acbb9a4206c64
Mathew Robinson <chasinglogic@gmail.com>
2017-05-17 16:18:05 -0400

    get origin url for parsing, prepare to make request

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate serde_json;
 extern crate toml;
 extern crate hyper;
 extern crate getopts;
+extern crate git2;
 
 use std::process;
 use std::path::PathBuf;
@@ -36,9 +37,14 @@ fn main() {
 
     // opts.optflag("g", "git", "Create the appropriate tags using git")
     opts.optflag("h", "help", "Show this help message.");
-    opts.reqopt("n", "name", "Name of the release.", "NAME");
+    opts.optopt("n", "name", "Name of the release. Defaults to \
+                              package name + version number as \
+                              defined in Cargo.toml. Example: \
+                              cargo-hublish v0.1.0",
+                "NAME");
     opts.optopt("t", "tag", "Name of the git tag for the release, \
-                             if not set defaults to `NAME`.",
+                             if not set defaults to version number as \
+                             defined in Cargo.toml.",
                 "TAG_NAME");
     opts.optopt("c", "commit", "SHA of the commit the tag should \
                                 point to, defaults to HEAD of master",
@@ -51,7 +57,18 @@ fn main() {
     opts.optflag("d", "draft", "Set whether this is a draft release \
                                 defaults to false");
     opts.optflag("p", "prerelease", "Set whether this is a prerelease \
-                                defaults to false");
+                                     defaults to false");
+    opts.optopt("u", "url", "URL for the github API request. \
+                              cargo-hublish attempts to find this \
+                              based on the origin url of the git repo. \
+                              If you're using a different remote such \
+                              as 'github' then use the --remote flag \
+                              to set that name, otherwise set the \
+                              full api url with this flag.",
+                 "URL");
+    opts.optopt("r", "remote", "Remote name to use when generating \
+                                 API endpoint. Defaults to origin.",
+                 "REMOTE");
 
     let matches = match opts.parse(&args) {
         Ok(m) => m,
@@ -59,17 +76,37 @@ fn main() {
     };
 
     if matches.opt_present("help") {
-        println!("Help!");
+        print!("{}", opts.usage("Usage: cargo hublish [options]"));
         process::exit(0)
     }
 
-    let rel_name = matches.opt_str("name").unwrap();
+    let project_root = match get_project_root() {
+        Some(r) => r,
+        None => {
+            println!("Command must be run inside a cargo project.");
+            process::exit(1)
+        }
+    };
+
+    let mut manifest = project_root.clone();
+    manifest.push("Cargo.toml");
+    let content = read_file(&manifest);
+    let cfg: Cargo = toml::from_str(&content).unwrap();
+    println!("cfg: {:?}", cfg);
+
+    let rel_name = if matches.opt_present("name") {
+        matches.opt_str("name")
+            .expect("name requires an argument")
+    } else {
+        format!("{} v{}", cfg.package.name,
+                cfg.package.version.clone())
+    };
 
     let tag_name = if matches.opt_present("tag") {
         matches.opt_str("tag_name")
             .expect("tag_name requires an argument")
     } else {
-        rel_name.clone()
+        cfg.package.version
     };
 
     let target_commit = if matches.opt_present("commit") {
@@ -88,18 +125,6 @@ fn main() {
         "".to_string()
     };
 
-    let project_root = get_project_root();
-    if project_root.is_none() {
-        println!("Command must be run inside a cargo project.");
-        process::exit(1)
-    }
-
-    let mut manifest = project_root.unwrap();
-    manifest.push("Cargo.toml");
-    let content = read_file(&manifest);
-
-    let cfg: Cargo = toml::from_str(&content).unwrap();
-    println!("cfg: {:?}", cfg);
 
     let rel = Release::new()
         .name(rel_name)
@@ -108,10 +133,28 @@ fn main() {
         .target_commitsh(target_commit)
         .prerelease(matches.opt_present("prerelease"))
         .draft(matches.opt_present("draft"));
+
     let json = serde_json::to_string(&rel).unwrap();
-    println!("json: {}", json)
+    println!("json: {}", json);
 
+    let repo = match git2::Repository::open(project_root) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error opening repo: {}", e);
+            process::exit(1)
+        }
+    };
 
-    let client = Client::new();
-    let res = client.post("https://api.github.com/chasinglogic/")
+    let origin = match repo.find_remote("origin") {
+        Ok(o) => o,
+        Err(e) => {
+            println!("Remote origin not set {}", e);
+            process::exit(1)
+        }
+    };
+
+    println!("{}", origin.url().unwrap())
+
+    // let client = Client::new();
+    // let res = client.post("https://api.github.com/");
 }
