34e74329f12a21769091aaffc3ea80b6355f107f
Mathew Robinson <chasinglogic@gmail.com>
2017-05-17 17:37:17 -0400

    send the request

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,14 @@ extern crate getopts;
 extern crate git2;
 
 use std::process;
+use std::io::Read;
 use std::path::PathBuf;
 use utils::get_project_root;
 use utils::read_file;
 use hyper::Client;
+use hyper::status::StatusCode;
 use release::Release;
+use release::ReleaseResponse;
 use getopts::Options;
 use std::env;
 
@@ -70,12 +73,12 @@ fn main() {
                                  API endpoint. Defaults to origin.",
                  "REMOTE");
 
-    let matches = match opts.parse(&args) {
+    let m = match opts.parse(&args) {
         Ok(m) => m,
         Err(e) => panic!(e.to_string()),
     };
 
-    if matches.opt_present("help") {
+    if m.opt_present("help") {
         print!("{}", opts.usage("Usage: cargo hublish [options]"));
         process::exit(0)
     }
@@ -94,32 +97,32 @@ fn main() {
     let cfg: Cargo = toml::from_str(&content).unwrap();
     println!("cfg: {:?}", cfg);
 
-    let rel_name = if matches.opt_present("name") {
-        matches.opt_str("name")
+    let rel_name = if m.opt_present("name") {
+        m.opt_str("name")
             .expect("name requires an argument")
     } else {
         format!("{} v{}", cfg.package.name,
                 cfg.package.version.clone())
     };
 
-    let tag_name = if matches.opt_present("tag") {
-        matches.opt_str("tag_name")
+    let tag_name = if m.opt_present("tag") {
+        m.opt_str("tag_name")
             .expect("tag_name requires an argument")
     } else {
         cfg.package.version
     };
 
-    let target_commit = if matches.opt_present("commit") {
-        matches.opt_str("commit")
+    let target_commit = if m.opt_present("commit") {
+        m.opt_str("commit")
             .expect("commit requires an argument")
     } else {
         "master".to_string()
     };
 
-    let body = if matches.opt_present("message") {
-        matches.opt_str("message").unwrap().to_string()
-    } else if matches.opt_present("file") {
-        let p = PathBuf::from(matches.opt_str("file").unwrap());
+    let body = if m.opt_present("message") {
+        m.opt_str("message").unwrap().to_string()
+    } else if m.opt_present("file") {
+        let p = PathBuf::from(m.opt_str("file").unwrap());
         read_file(&p)
     } else {
         "".to_string()
@@ -131,8 +134,8 @@ fn main() {
         .tag_name(tag_name)
         .body(body)
         .target_commitsh(target_commit)
-        .prerelease(matches.opt_present("prerelease"))
-        .draft(matches.opt_present("draft"));
+        .prerelease(m.opt_present("prerelease"))
+        .draft(m.opt_present("draft"));
 
     let json = serde_json::to_string(&rel).unwrap();
     println!("json: {}", json);
@@ -145,7 +148,8 @@ fn main() {
         }
     };
 
-    let origin = match repo.find_remote("origin") {
+    let remote_name = m.opt_str("remote").unwrap_or("origin".to_string());
+    let origin = match repo.find_remote(&remote_name) {
         Ok(o) => o,
         Err(e) => {
             println!("Remote origin not set {}", e);
@@ -153,8 +157,38 @@ fn main() {
         }
     };
 
-    println!("{}", origin.url().unwrap())
+    let url = if m.opt_present("url") {
+        m.opt_str("url")
+            .expect("url requires an argument")
+    } else {
+        let u = origin.url().expect("Unexpected error.");
+        let mut spl: Vec<&str> = u.split('/').collect();
+        let mut base = "https://api.github.com/".to_string();
+        let repo_name = spl.pop().unwrap();
+        let username = spl.pop().unwrap();
+        let endpoint = format!("{}/{}/releases", username, repo_name);
+        base.push_str(&endpoint);
+        base
+    };
 
-    // let client = Client::new();
-    // let res = client.post("https://api.github.com/");
+    println!("url: {}", url);
+
+    let client = Client::new();
+    let mut res = client.post(&url)
+        .body(&json)
+        .send()
+        .expect("Error calling github API.");
+
+    let mut buf = String::new();
+    res.read_to_string(&mut buf)
+        .expect("Unable to read Github response.");
+
+    if res.status >= StatusCode::MultipleChoices {
+        println!("ERROR Status: {} Response: {}", res.status, buf);
+        process::exit(1)
+    }
+
+    let rr: ReleaseResponse = serde_json::from_str(&buf).unwrap();
+    println!("Successfully created release!
+View it here: {}", rr.html_url);
 }
