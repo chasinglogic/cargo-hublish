269795dff30fab5121eeb670ebc54c2cdccd2346
Mathew Robinson <chasinglogic@gmail.com>
2017-05-17 15:08:05 -0400

    define model for github release API

---
+++ src/release.rs
@@ -0,0 +1,53 @@
+#[derive(Serialize, Debug, Default)]
+pub struct Release {
+    pub tag_name: String,
+    pub target_commitsh: String,
+    pub name: String,
+    pub body: String,
+    pub draft: bool,
+    pub prerelease: bool,
+}
+
+// Builder pattern
+impl Release {
+    pub fn new() -> Release {
+        Release {
+            body: "".to_string(),
+            name: "".to_string(),
+            tag_name: "".to_string(),
+            target_commitsh: "master".to_string(),
+            draft: false,
+            prerelease: false,
+        }
+    }
+
+    pub fn name(mut self, name: String) -> Release {
+        self.name = name;
+        self
+    }
+
+    pub fn body(mut self, body: String) -> Release {
+        self.body = body;
+        self
+    }
+
+    pub fn tag_name(mut self, tag_name: String) -> Release {
+        self.tag_name = tag_name;
+        self
+    }
+
+    pub fn target_commitsh(mut self, target_commitsh: String) -> Release {
+        self.target_commitsh = target_commitsh;
+        self
+    }
+
+    pub fn prerelease(mut self, prerelease: bool) -> Release {
+        self.prerelease = prerelease;
+        self
+    }
+
+    pub fn draft(mut self, draft: bool) -> Release {
+        self.draft = draft;
+        self
+    }
+}
